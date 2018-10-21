de6cc3ec7fdcf37181992a767cb06aca72264334
Stephen Fox <stephen.j.fox.jr@gmail.com>
2018-10-21 12:04:21 -0400

    Moved settings dir functions into separate file.

---
+++ internal/settings/dir.go
@@ -0,0 +1,44 @@
+package settings
+
+import (
+	"os"
+	"strings"
+	"path"
+	"runtime"
+)
+
+const (
+	defaultDirMode         = 0755
+	internalDirName        = ".internal"
+	defaultSettingsDirname = ".grundy"
+)
+
+func DirPath() string {
+	var parentPath string
+
+	switch runtime.GOOS {
+	case "darwin":
+		fallthrough
+	case "linux":
+		parentPath = os.Getenv("HOME")
+	case "windows":
+		parentPath = strings.Replace(os.Getenv("USERPROFILE"), "\\", "/", -1)
+	}
+
+	return path.Join(parentPath, defaultSettingsDirname)
+}
+
+func CreateInternalDirPath(settingsDirPath string) (string, error) {
+	dirPath := path.Join(settingsDirPath, internalDirName)
+
+	err := CreateDir(dirPath)
+	if err != nil {
+		return "", err
+	}
+
+	return dirPath, nil
+}
+
+func CreateDir(dirPath string) error {
+	return os.MkdirAll(dirPath, defaultDirMode)
+}
